d4e6ea51da55bc6426d087d026b047456dd8b3b4
goki sakaguchi <goki0524@gmail.com>
2019-09-07 23:09:37 +0900

    finish 159

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+
+  final String CategoryId;
+  final String CategoryTitle;
+
+  CategoryMealsScreen(this.CategoryId, this.CategoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(CategoryTitle),
+      ),
+      body: Center(
+        child: Text(
+          'The Recipes For The Category!',
+        ),
+      ),
+    );
+  }
+}
