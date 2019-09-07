02d2a2e6664504b26bbe3efd32787d6a0d11d87e
goki sakaguchi <goki0524@gmail.com>
2019-09-07 23:25:57 +0900

    finish 160

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  // final String CategoryId;
+  // final String CategoryTitle;
 
-  final String CategoryId;
-  final String CategoryTitle;
-
-  CategoryMealsScreen(this.CategoryId, this.CategoryTitle);
+  // CategoryMealsScreen(this.CategoryId, this.CategoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(CategoryTitle),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
